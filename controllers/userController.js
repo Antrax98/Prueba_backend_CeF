5c3ea73ce4ad45e2c5dd9c480d718597533c0a0a
Antrax98 <kazapox@gmail.com>
2023-07-31 23:22:02 -0400

    cargo dentro de token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,52 @@
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
+const Ficha = require('../models/fichaModel')
+const Temporada = require('../models/temporadaModel')
 
 const createToken = async (user) => {
+
+    //obtener temporada actual
+    let tempoAct
+
+    try{
+        let fechaAct = new Date()
+        tempoAct = await Temporada.findOne({inicio: {$lte: fechaAct.getTime()}, fin: {$gte: fechaAct.getTime()}})
+    }catch(error){
+        tempoAct = null
+        console.log("NO SE PUDO ENCONTREAR TEMPORADA ACTUAL EN TOKEN LOGIN")
+    }
+
+    //FIND si user tiene fichaactual
+    let fichaAct
+    try{
+        if(tempoAct){
+            fichaAct = await Ficha.findOne({user: user._id,temporada: tempoAct._id})
+        }else{
+            fichaAct = null
+        }
+    }catch(error){
+        fichaAct = null
+        console.log("NO SE PUDO ENCONTRAR UNA FICHA EN LA TEMPORADA ACTUAL")
+    }
+
+    //AGREGANDO CARGO DE LA TEMPORADA
+    //NULL SI NO TIENE O ES ADMIN
+    let cargo
+
+    if(fichaAct){
+        cargo = fichaAct.cargo
+    }else{
+        cargo = null
+    }
+
 
     return jwt.sign(
         {
             _id : user._id,
             userType : user.userType,
             nombres: user.nombres,
-            apellidos: user.apellidos
+            apellidos: user.apellidos,
+            cargo: cargo
         },
         process.env.SECRET,
         {
